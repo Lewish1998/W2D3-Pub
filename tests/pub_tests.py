1aa54825615cae6840c290d4258031d76ed0a5da
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-01-18 21:24:04 +0000

    Comitting before channging food to dict (pub.py)

--- tests/pub_tests.py
+++ tests/pub_tests.py
@@ -42,6 +42,18 @@ class TestPub(unittest.TestCase):
         drink = Drinks('Moonshine', 3.50, 20)
         self.assertEqual(False, self.customer.buy_drink(drink=drink, pub=self.pub))
         
-    # def test_buy_food(self):
-    #     self.customer.buy_food(self.pub, self.food)
-        
+    def test_buy_food(self):
+        self.customer.buy_food(self.food, self.pub)
+        self.assertEqual(77.25 ,self.customer.wallet)
+        self.assertEqual(502.75 ,self.pub.till)
+
+    def test_drunk_level_after_food(self):
+        self.customer.buy_drink(drink=self.drink, pub=self.pub)
+        self.assertEqual(2 ,self.customer.drunkenness_level)
+        self.customer.buy_food(self.food, self.pub)
+        self.assertEqual(0 ,self.customer.drunkenness_level)
+
+    
+
+
+    
